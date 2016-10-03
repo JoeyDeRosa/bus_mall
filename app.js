169ea355589329eab665e912958427e3f167f4ea
Joey DeRosa <joeyderosa11.jd@gmail.com>
2016-10-03 16:23:03 -0700

    no repeat or duplicit images

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 'use strict';
 //creating image objects and arrays
 var imageHolder = document.getElementById('imageHolder');
+var imgEl1 = document.createElement('img');
+var imgEl2 = document.createElement('img');
+var imgEl3 = document.createElement('img');
+var previousImage1 = false;
+var previousImage2 = false;
+var previousImage3 = false;
+var index = 0;
 var bag = new Image();
 bag.src = 'images/bag.jpg';
 var banana = new Image();
@@ -9,7 +16,7 @@ var bathroom = new Image();
 bathroom.src = 'images/bathroom.jpg';
 var boots = new Image();
 boots.src = 'images/boots.jpg';
-var breakfast= new Image();
+var breakfast = new Image();
 breakfast.src = 'images/breakfast.jpg';
 var bubblegum = new Image();
 bubblegum.src = 'images/bubblegum.jpg';
@@ -42,17 +49,58 @@ water.src = 'images/water-can.jpg';
 var wine = new Image();
 wine.src = 'images/wine-glass.jpg';
 var images = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dog, dragon, pen, pet, scissors, shark, sweep, tauntaun, unicorn, usb, water, wine];
-var imageSelected = [];
-var imageTimesShown = [];
+var imageSelected = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var imageTimesShown = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 //creating functions
 function randomNumber() {
-  return Math.floor(Math.random() * images.length);
+  index = Math.floor(Math.random() * images.length);
+  return index;
 }
 
-for(var i = 0; i < 3; i++) {
-  var imgEl = document.createElement('img');
-  imgEl.src = images[randomNumber()].src;
-  console.log(imgEl);
-  imageHolder.appendChild(imgEl);
+function selectImage() {
+  return images[randomNumber()].src;
 }
+
+function generateImages() {
+  var image1 = selectImage();
+  while(image1 === previousImage1 || image1 === previousImage2 || image1 === previousImage3) {
+    image1 = selectImage();
+  }
+  console.log(image1);
+  console.log(previousImage1);
+  ++imageTimesShown[index];
+  imgEl1.src = image1;
+  imageHolder.appendChild(imgEl1);
+  //
+  var image2 = selectImage();
+  while(image2 === previousImage1 || image2 === previousImage2 || image2 === previousImage3 || image2 === image1) {
+    image2 = selectImage();
+  }
+  console.log(image2);
+  console.log(previousImage2);
+  ++imageTimesShown[index];
+  imgEl2.src = image2;
+  imageHolder.appendChild(imgEl2);
+  //
+  var image3 = selectImage();
+  while(image3 === previousImage1 || image3 === previousImage2 || image3 === previousImage3 || image3 === image1 || image3 === image2) {
+    image3 = selectImage();
+  }
+  console.log(image3);
+  console.log(previousImage3);
+  ++imageTimesShown[index];
+  imgEl3.src = image3;
+  imageHolder.appendChild(imgEl3);
+  previousImage1 = image1;
+  previousImage2 = image2;
+  previousImage3 = image3;
+}
+
+function handleClick() {
+  generateImages();
+}
+
+//run functions
+generateImages();
+imageHolder.addEventListener('click', handleClick);
